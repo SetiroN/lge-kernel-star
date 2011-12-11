ab46e15f21e05fe580c54f8a4f99842eab7c77f3
SetiroN <setiron.hypnotoad@gmail.com>
2011-12-11 07:42:33 +0100

    Streamline default config and tweaks
    Conflicts:
    
    	arch/arm/configs/setiron_defconfig

--- include/linux/ironkrnl.h
+++ include/linux/ironkrnl.h
@@ -6,21 +6,21 @@
 #define IRON_AVP_FREQ 240000
 #define IRON_VDE_FREQ IRON_AVP_FREQ
 #define IRON_SYSTEM_FREQ IRON_AVP_FREQ
-#define IRON_EMC_VOLT 1200
-#define IRON_GPU_FREQ 400000
+#define IRON_EMC_VOLT 1250
+#define IRON_GPU_FREQ 300000
 #define IRON_EMC2_FREQ ( IRON_VDE_FREQ * 5 / 2 )
 #define IRON_DDR_FREQ ( IRON_EMC2_FREQ / 2 )
 
-#define IRON_GPU_DIVIDER (7)
+#define IRON_GPU_DIVIDER (8)
 
 #define IRON_PLLA0 11289
 #define IRON_PLLX0 1000000
 
 #define IRON_BOOT_CLOCK 1312000
 
-#define IRONVOLT_216 775
-#define IRONVOLT_503 800
-#define IRONVOLT_655 825
+#define IRONVOLT_216 800
+#define IRONVOLT_503 825
+#define IRONVOLT_655 850
 #define IRONVOLT_912 900
 #define IRONVOLT_1100 1000
 #define IRONVOLT_1312 1075
@@ -46,6 +46,6 @@
 #define sched_nr_latency_default 8
 #define sysctl_sched_wakeup_granularity_default 1000000UL
 #define normalized_sysctl_sched_wakeup_granularity_default 1000000UL
-#define CFS_BOOST
+//#define CFS_BOOST
 #define CFS_BOOST_NICE -19
 
